10130b4511979a64b143c9f7ab5f2c1324596eb4
MrSven86 <tomas.gustav.eriksson@gmail.com>
2026-04-06 16:37:43 +0200

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
   try {
     const data = await resend.emails.send({
-      from: 'Contact Form <onboarding@resend.dev>',
+      from: 'Contact Form <noreply@velocityweb.org>',
       to: clientEmail || 'tomas.gustav.eriksson@gmail.com',
       subject: `New Contact from ${name}${website ? ` - ${website}` : ''}`,
       html: `
